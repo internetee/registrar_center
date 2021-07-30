61ea7f0bdd39f866ab58615ab60b364707a5874e
Alex Sherman <yul.golem@gmail.com>
2021-07-30 13:31:30 +0500

    Add bulk tech contact replace & tests

---
+++ app/services/api_connector/bulk_actions/tech_contact_replace.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class ApiConnector
+  module BulkActions
+    class TechContactReplace < ApiConnector
+      include EndpointRequestable
+      ACTION = 'replace_contacts'
+      ENDPOINT = {
+        method: 'patch',
+        endpoint: '/domains/contacts',
+      }.freeze
+
+      def replace_contacts(payload: nil)
+        request(url: endpoint_url, method: method, params: transfer_params(payload))
+      end
+
+      private
+
+      def transfer_params(payload)
+        {
+          current_contact_id: payload[:current_contact_id],
+          new_contact_id: payload[:new_contact_id]
+        }
+      end
+    end
+  end
+end
