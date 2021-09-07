39cd538259af8435472eaf63f59063e5a1988d5a
Alex Sherman <yul.golem@gmail.com>
2021-09-07 12:54:55 +0500

    WIP interim save

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+class MessagesController < ApplicationController
+  # Get all the messages
+  def index
+
+  end
+
+  # Get a specific message
+  def show
+
+  end
+
+  # Get latest unread message
+  def latest
+
+  end
+
+  # Mark message as read
+  def edit
+
+  end
+
+  def update
+
+  end
+
+end
