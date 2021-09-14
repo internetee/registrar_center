df07b515d9e265563512471f28897f5fd9c2540f
Alex Sherman <yul.golem@gmail.com>
2021-09-14 14:45:17 +0500

    Add failed auth redirect tests

--- spec/support/base_controller_shared_examples.rb
+++ spec/support/base_controller_shared_examples.rb
@@ -1,13 +1,16 @@
 RSpec.shared_examples "Base controller with auth" do |options|
   let(:uuid) { Faker::Internet.uuid }
-  let(:username) { Faker::Lorem.word }
-  let(:password) { Faker::Internet.password }
-  let(:auth_data)  { { username: username, password: password } }
+  let(:username) { 'test_bestnames' }
+  let(:username_fail) { Faker::Lorem.word }
+  let(:password) { 'testtest' }
+  let(:password_fail) { Faker::Internet.password }
+  let(:auth_data_legal)  { { username: username, password: password } }
+  let(:auth_data_fail)  { { username: username_fail, password: password_fail } }
 
   options.each do |option|
     it "successfully checks for auth info in session" do
       session[:uuid] = uuid
-      Rails.cache.write(uuid, auth_data)
+      Rails.cache.write(uuid, auth_data_legal)
       VCR.use_cassette("#{described_class}-#{option[:method]}") do
         self.send(option[:http_method], option[:method], params: option[:params])
       end
@@ -27,6 +30,17 @@ RSpec.shared_examples "Base controller with auth" do |options|
       self.send(option[:http_method], option[:method], params: option[:params])
 
       expect(response.status).to eq(302)
+    end
+
+    it "Redirects to auth controller if failed auth" do
+      session[:uuid] = uuid
+      Rails.cache.write(uuid, auth_data_fail)
+
+      VCR.use_cassette("#{described_class}-#{option[:method]}-auth-fail") do
+        self.send(option[:http_method], option[:method], params: option[:params])
+      end
+
+      expect(response).to redirect_to(controller: 'sessions', action: 'new')
     end
   end
 end
